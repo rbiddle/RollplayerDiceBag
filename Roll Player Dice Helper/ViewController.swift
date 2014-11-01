d3370263843f2d7623677d7f5959fed799007bed
Robert Biddle <robert.biddle@gmail.com>
2014-11-01 14:04:34 -0500

    Initial Commit

---
+++ Roll Player Dice Helper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roll Player Dice Helper
+//
+//  Created by Robert Biddle on 11/1/14.
+//  Copyright (c) 2014 Robert Biddle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Roll Player Dice HelperTests/Roll_Player_Dice_HelperTests.swift
@@ -0,0 +1,36 @@
+//
+//  Roll_Player_Dice_HelperTests.swift
+//  Roll Player Dice HelperTests
+//
+//  Created by Robert Biddle on 11/1/14.
+//  Copyright (c) 2014 Robert Biddle. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Roll_Player_Dice_HelperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
